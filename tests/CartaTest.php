95d9cdb0014a24ee0e21d4128cc2f4dabf9cae0f
Manuel Lopez <manuelopez.30@hotmail.com>
2018-11-21 18:48:37 -0300

    Completado todos los test de CartaTest

--- tests/CartaTest.php
+++ tests/CartaTest.php
@@ -6,4 +6,24 @@ use PHPUnit\Framework\TestCase;
 
     class CartaTest extends TestCase {
 
+        /**
+     * Valida que se puedan crear cartas.
+     */
+    public function testExiste() {
+        $carta = new Carta("2","PALO";
+        $this->assertTrue(isset($carta));
+    }
+    /** Valida que se puede obtener
+     * el numero de una carta
+     */
+	public function testVerNumero(){
+		$carta = new Carta("7","ESPADA");
+		$this->assertEquals($carta->verNumero(),7);
+    }
+    /** Valida que se puede ver el palo de una carta
+     */
+    public function testVerPalo(){
+		$carta = new Carta("5","COPA");
+		$this->assertEquals($carta->verPalo(),"COPA");
+	}
 }
