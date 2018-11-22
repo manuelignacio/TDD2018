f79b5b2ca9b619bbf03eb266ffa1da3f8490ba8d
Manuel Lopez <manuelopez.30@hotmail.com>
2018-11-22 20:26:20 -0300

    Corrigiendo errores

--- src/Carta.php
+++ src/Carta.php
@@ -7,30 +7,11 @@ class Carta {
 protected $numero;
 protected $palo;
 
-public function constructor(int $numero,string $palo){
-if($this->PaloValido($palo)){
+public function constructor($numero, $palo){
   $this->palo = $palo;
-}
-else{
-  $this->palo="Espada";
-}
-if($numero <= 12 && $numero > 0){
   $this->numero = $numero;
 }
-else{
-  $this->numero = 1;
-}
-}
-public function PaloValido($palo){
-switch(strtolower($palo)){
-    case "COPA":
-    case "PALO":
-    case "ESPADA":
-    case "ORO":
-      return TRUE;
-  }
-  return FALSE;
-}
+
 public function verPalo(){
   return $this->palo;
 }
