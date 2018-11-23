c21d80c44ff0af40b12f277260677c73fefdd971
Tadeo Schlieper <tadeo.sch@hotmail.com>
2018-11-23 03:03:07 -0300

    Agregados los tipos de carta, calculado en el constructor de Carta

--- src/Carta.php
+++ src/Carta.php
@@ -6,17 +6,41 @@ class Carta {
 
     protected $numero;
     protected $palo;
+    protected $tipo;
 
     public function __construct(string $numero, string $palo){
         $this->palo = $palo;
         $this->numero = $numero;
+
+        $numeroEspañola = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"];
+        $paloEspañola = ["ORO", "ESPADA", "COPA", "PALO", "BASTO"];
+        $esEspañola = in_array($numero, $numeroEspañola) && in_array($palo, $paloEspañola);
+
+        $numeroPoker = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+        $paloPoker = ["PICA", "TREBOL", "DIAMANTE", "CORAZON"];
+        $esPoker = in_array($numero, $numeroPoker) && in_array($palo, $paloPoker);
+
+        if ($esEspañola) {
+            $this->tipo = "Española";
+        }
+        elseif ($esPoker) {
+            $this->tipo = "Poker";
+        }
+        else {
+            $this->tipo = "Otra";
+        }
+    }
+
+    public function verNumero(){
+        return $this->numero;
     }
 
     public function verPalo(){
         return $this->palo;
     }
-    public function verNumero(){
-        return $this->numero;
+
+    public function obtenerTipo() {
+        return $this->tipo;
     }
 
 }
